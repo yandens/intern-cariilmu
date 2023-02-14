1391afd94771aae772939da07ebdb3481aab4c0e
Wayan Deden Setyawan <dedensetyawan17120104@gmail.com>
2023-02-14 12:36:36 +0800

    repair error handler

--- src/app.js
+++ src/app.js
@@ -14,7 +14,7 @@ app.use(bodyParser.urlencoded({ extended: false }));
 
 app.use("/api", routes);
 
-app.use((req, res, nextc) => {
+app.use((req, res, next) => {
   return response(res, 404, false, "Are you lost?", null);
 });
 
