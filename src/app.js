21750577af7453fc6c586d73ce327c37195e6ddd
Wayan Deden Setyawan <dedensetyawan17120104@gmail.com>
2023-02-14 12:15:21 +0800

    add routes and reformat error handling

--- src/app.js
+++ src/app.js
@@ -2,27 +2,26 @@ require("dotenv").config();
 const express = require("express");
 const bodyParser = require("body-parser");
 const morgan = require("morgan");
+const routes = require("./routes");
+const response = require("./utils/response");
 const { PORT } = process.env;
+
 const app = express();
 
-app.use(morgan("dev")); // for logging
-app.use(express.json()); // read body type json
+app.use(morgan("dev"));
+app.use(express.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
-app.use((req, res, next) => {
-  return res.status(404).json({
-    status: false,
-    message: "Are you lost?",
-  });
+app.use("/api", routes);
+
+app.use((req, res, nextc) => {
+  return response(res, 404, false, "Are you lost?", null);
 });
 
 // 500 handler
 app.use((err, req, res, next) => {
   console.log(err);
-  return res.status(500).json({
-    status: false,
-    message: err.message,
-  });
+  return response(res, 500, false, err.message, null);
 });
 
 app.listen(PORT, () => console.log(`Listening on port ${PORT}`));
